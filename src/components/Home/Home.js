411c55eb2127d86d11dddb49a33fa6fe6a7b228f
shira471 <shalfasi@g.jct.ac.il>
2025-05-26 13:56:42 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import './Home.css';
 
 const Home=()=>(
     <section className='Home'>
-    <img src="/profile-image.jpg" className="profile"/>
+    <img src={process.env.PUBLIC_URL + "/profile-image.jpg"} className="profile" alt="My profile" />
     <h1>shira alfasi</h1>
     <p>Passionate software developer with a solid foundation in coding and a background in biological sciences. Proficient in software engineering principles, including SOLID design patterns and Object-Oriented Programming.</p>
   </section>
