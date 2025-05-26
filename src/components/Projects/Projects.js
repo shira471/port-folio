b8f3252be418d48fb97c6897a4fc47123289eb91
shira alfasi <shalfasi@g.jct.ac.il>
2025-05-26 14:44:34 +0300

    j

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,28 +8,28 @@ const Projects = () => {
     {
       title: "Resume Website – Personal Portfolio in React",
       description:
-        "Developed a personal website using React to showcase my resume in English...",
+        "Developed a personal website using React to showcase my resume in English. The site features a clean, responsive user interface and presents my background and skills in a professional and accessible manner.",
       link: "https://github.com/shira471/port-folio",
       videoUrl: baseUrl + "/website-video.mp4",
     },
     {
       title: "Cake Recognition App – AI-powered cake identification and allergen detection",
       description:
-        "Designed and implemented a mobile app...",
+        "Designed and implemented a mobile app that lets users upload a cake image for analysis, identification, and allergen detection using a trained CNN built with TensorFlow. Developed with a React Native client, a Python server, and a CNN trained on a Kaggle dataset, communicating via a REST API.",
       link: "https://github.com/shira471/cake-allergene",
       videoUrl: baseUrl + "/cake-video.mp4",
     },
     {
       title: "Community Volunteering Management - Desktop Application",
       description:
-        "Developed a desktop application to manage help requests...",
+        "Developed a desktop application to manage help requests and volunteers. The application enables volunteers to log in, browse available requests, and select the ones they wish to assist with. Built with C# and WPF, the application has a three-layer architecture and uses Singleton and Factory design patterns.",
       link: "https://github.com/shira471/dotNet5785_4033_4273",
       videoUrl: baseUrl + "/volunteer-video.mp4",
     },
     {
       title: "Bioinformatics Project on Parkinson's Disease",
       description:
-        "Developed a Python tool to identify drugs and molecules related to Parkinson's...",
+        "Developed a Python tool to identify drugs and molecules related to Parkinson's disease using NCBI data mining techniques. Created algorithms for automated data extraction and analysis. Supervised by Dr. Avraham Samson, this project will be published in Nature magazine.",
     },
   ];
 
