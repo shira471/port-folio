8557d5e2fb1285531deb77e8e76f74a99cb73d7f
shira alfasi <shalfasi@g.jct.ac.il>
2025-05-27 11:43:07 +0300

    s

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,38 +1,36 @@
 import React from "react";
 import "./Projects.css";
 
+const projects = [
+  {
+    title: "Resume Website – Personal Portfolio in React",
+    description:
+      "Developed a personal website using React to showcase my resume in English. The site features a clean, responsive user interface and presents my background and skills in a professional and accessible manner.",
+    link: "https://github.com/shira471/port-folio",
+    videoUrl: "/port-folio/website-video.mp4",
+  },
+  {
+    title: "Cake Recognition App – AI-powered cake identification and allergen detection",
+    description:
+      "Designed and implemented a mobile app that lets users upload a cake image for analysis, identification, and allergen detection using a trained CNN built with TensorFlow. Developed with a React Native client, a Python server, and a CNN trained on a Kaggle dataset, communicating via a REST API.",
+    link: "https://github.com/shira471/cake-allergene",
+    videoUrl: "/poty-folio/cake-video.mp4", 
+  },
+  {
+    title: "Community Volunteering Management - Desktop Application",
+    description:
+      "Developed a desktop application to manage help requests and volunteers. The application enables volunteers to log in, browse available requests, and select the ones they wish to assist with. Built with C# and WPF, the application has a three-layer architecture and uses Singleton and Factory design patterns.",
+    link: "https://github.com/shira471/dotNet5785_4033_4273",
+    videoUrl: "/port-folio/volunteer-video.mp4",
+  },
+  {
+    title: "Bioinformatics Project on Parkinson's Disease",
+    description:
+      "Developed a Python tool to identify drugs and molecules related to Parkinson's disease using NCBI data mining techniques. Created algorithms for automated data extraction and analysis. Supervised by Dr. Avraham Samson, this project will be published in Nature magazine.",
+  },
+];
+
 const Projects = () => {
-  const baseUrl = "https://shira471.github.io/port-folio";
-
-  const projects = [
-    {
-      title: "Resume Website – Personal Portfolio in React",
-      description:
-        "Developed a personal website using React to showcase my resume in English. The site features a clean, responsive user interface and presents my background and skills in a professional and accessible manner.",
-      link: "https://github.com/shira471/port-folio",
-      videoUrl: baseUrl+"/website-video.mp4",
-    },
-    {
-      title: "Cake Recognition App – AI-powered cake identification and allergen detection",
-      description:
-        "Designed and implemented a mobile app that lets users upload a cake image for analysis, identification, and allergen detection using a trained CNN built with TensorFlow. Developed with a React Native client, a Python server, and a CNN trained on a Kaggle dataset, communicating via a REST API.",
-      link: "https://github.com/shira471/cake-allergene",
-      videoUrl:baseUrl+ "/cake-video.mp4",
-    },
-    {
-      title: "Community Volunteering Management - Desktop Application",
-      description:
-        "Developed a desktop application to manage help requests and volunteers. The application enables volunteers to log in, browse available requests, and select the ones they wish to assist with. Built with C# and WPF, the application has a three-layer architecture and uses Singleton and Factory design patterns.",
-      link: "https://github.com/shira471/dotNet5785_4033_4273",
-      videoUrl: baseUrl+"/volunteer-video.mp4",
-    },
-    {
-      title: "Bioinformatics Project on Parkinson's Disease",
-      description:
-        "Developed a Python tool to identify drugs and molecules related to Parkinson's disease using NCBI data mining techniques. Created algorithms for automated data extraction and analysis. Supervised by Dr. Avraham Samson, this project will be published in Nature magazine.",
-    },
-  ];
-
   return (
     <section className="projects-container">
       <div className="projects-row">
@@ -46,9 +44,10 @@ const Projects = () => {
           >
             <h2 className="project-title">{project.title}</h2>
             <p className="project-description">{project.description}</p>
-            {project.videoUrl && (
+
+                {project.videoUrl && (
               <video className="project-video" controls>
-                <source src={project.videoUrl} type="video/mp4" />
+                <source src={`${process.env.PUBLIC_URL}${project.videoUrl}`} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             )}
@@ -58,5 +57,6 @@ const Projects = () => {
     </section>
   );
 };
+
 
 export default Projects;
