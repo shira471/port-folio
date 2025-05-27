b4dab7107d590ac8072ffc8c6deefcdb7d96f35a
shira alfasi <shalfasi@g.jct.ac.il>
2025-05-27 12:12:38 +0300

    h

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,21 +7,21 @@ const projects = [
     description:
       "Developed a personal website using React to showcase my resume in English. The site features a clean, responsive user interface and presents my background and skills in a professional and accessible manner.",
     link: "https://github.com/shira471/port-folio",
-    videoUrl: "/media/website-video.mp4",
+    videoUrl: "/website-video.mp4",
   },
   {
     title: "Cake Recognition App – AI-powered cake identification and allergen detection",
     description:
       "Designed and implemented a mobile app that lets users upload a cake image for analysis, identification, and allergen detection using a trained CNN built with TensorFlow. Developed with a React Native client, a Python server, and a CNN trained on a Kaggle dataset, communicating via a REST API.",
     link: "https://github.com/shira471/cake-allergene",
-    videoUrl: "/media/cake-video.mp4", 
+    videoUrl: "/cake-video.mp4", 
   },
   {
     title: "Community Volunteering Management - Desktop Application",
     description:
       "Developed a desktop application to manage help requests and volunteers. The application enables volunteers to log in, browse available requests, and select the ones they wish to assist with. Built with C# and WPF, the application has a three-layer architecture and uses Singleton and Factory design patterns.",
     link: "https://github.com/shira471/dotNet5785_4033_4273",
-    videoUrl: "/media/volunteer-video.mp4",
+    videoUrl: "/volunteer-video.mp4",
   },
   {
     title: "Bioinformatics Project on Parkinson's Disease",
