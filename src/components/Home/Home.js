b4ad4a93fa2ab9b7ab355b6e082871a27b5cab65
shira alfasi <shalfasi@g.jct.ac.il>
2025-05-27 19:48:40 +0300

    f

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,8 @@ const Home=()=>(
     <section className='Home'>
     <img src= "/port-folio/profile-image.jpg" className="profile" alt="My profile" />
     <h1>shira alfasi</h1>
+    <h2>software developer</h2>
+    <h3>Holds a B.Sc. in Bioinformatics.</h3>
     <p>Passionate software developer with a solid foundation in coding and a background in biological sciences. Proficient in software engineering principles, including SOLID design patterns and Object-Oriented Programming.</p>
   </section>
 );
